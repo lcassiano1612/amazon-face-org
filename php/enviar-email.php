efae216eaf47ffda6bc1f4e08346ccaa83bb7546
Lucas de Oliveira <l.cassiano1612@gmail.com>
2018-10-21 19:53:03 -0300

    email

--- php/enviar-email.php
+++ php/enviar-email.php
@@ -1,11 +1,46 @@
 <?php
+$name = $_POST['nome'];
+//pega os dados que foi digitado no ID name.
 
-  $nome =     $_POST['nome'];
-  $email =    $_POST['email'];
-  $telefone = $_POST['telefone'];
-  $mensagem = $_POST['mensagem'];
+$email = $_POST['email'];
+//pega os dados que foi digitado no ID email.
 
-  echo "$nome $email $telefone $mensagem";
+$telefone = $_POST['telefone'];
+//pega os dados que foi digitado no ID message.
 
+$message = $_POST['mensagem'];
+//pega os dados que foi digitado no ID message.
 
+$subject = 'Formulario de contato do site';
+//pega os dados que foi digitado no ID sebject.
+
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+/*abaixo contém os dados que serão enviados para o email
+cadastrado para receber o formulário*/
+
+$corpo = "Formulário enviado\n";
+$corpo .= "Nome: " . $name . "\n";
+$corpo .= "Email: " . $email . "\n";
+$corpo .= "Telefone: " . $telefone . "\n";
+$corpo .= "Comentários: " . $message . "\n";
+
+$email_to = 'contato@amazon-face.org';
+//não esqueça de substituir este email pelo seu.
+
+$status = mail($email_to, $subject, $corpo, $headers);
+//enviando o email.
+
+if ($status) {
+  echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+  
+//mensagem de form enviado com sucesso.
+
+} else {
+  echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+  
+//mensagem de erro no envio. 
+
+}
 ?>
