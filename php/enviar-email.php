bc791cdb1d631c30e6902401ba6ddb61b818a3b1
Lucas de Oliveira <l.cassiano1612@gmail.com>
2018-10-21 19:40:04 -0300

    varias coisas

---
+++ php/enviar-email.php
@@ -0,0 +1,11 @@
+<?php
+
+  $nome =     $_POST['nome'];
+  $email =    $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+
+  echo "$nome $email $telfone $mensagem";
+
+
+?>
